7a23c6c5407f1a2c6cc013f410c87909b2c5253b
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-01 09:34:47 +0000

    building the navbar component

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,15 @@
 import Head from "next/head";
 import { Box } from "@chakra-ui/react";
-
+import Navbar from "./Navbar";
 const Layout = ({ children }) => (
   <>
     <Head>
       <title>Real Estate</title>
     </Head>
     <Box maxWidth="1280px" m="auto">
-      <header>navbar</header>
+      <header>
+        <Navbar />
+      </header>
       <main>{children}</main>
     </Box>
   </>
