4113094394b123c6a828871b1a8848eb1070a732
yassine.chtiaa@etu.uae.ac.ma <yassine.chtiaa@etu.uae.ac.ma>
2022-04-01 09:46:42 +0000

    build the footer for the app

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+const Footer = () => (
+  <Box
+    textAlign="center"
+    p="5"
+    color="gray.500"
+    borderTop="1px"
+    borderColor="gray.100"
+  >
+    2021 , Realtor Inc
+  </Box>
+);
+export default Footer;
